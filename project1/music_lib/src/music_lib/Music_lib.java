1b84d3f48091a694e2048430b9c84a040b861de1
anmolkhanna93 <anmolkhanna93@gmail.com>
2016-11-18 20:17:43 -0800

    Music Management

---
+++ project1/music_lib/src/music_lib/Music_lib.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package music_lib;
+
+/**
+ *
+ * @author satellite
+ */
+public class Music_lib {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
